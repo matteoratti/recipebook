2361029df659913e7b1e71fa864cb7d2329c5ddb
jpsevern <matteoratti90@gmail.com>
2022-07-13 13:23:25 +0200

    name validation on Tag and testing

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,6 @@
 
 class Tag < ApplicationRecord
   belongs_to :taggable, polymorphic: true
+
+  validates :name, presence: true
 end
--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -3,7 +3,40 @@
 require 'test_helper'
 
 class TagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @tag = Tag.new(name: 'cucina italiana', taggable: recipes(:carbonara))
+  end
+
+  test 'should be invalid without a name' do
+    @tag.name = nil
+    assert_not @tag.valid?
+  end
+
+  test 'should be invalid without a taggable' do
+    @tag.taggable = nil
+
+    assert_not @tag.valid?
+  end
+
+  test 'should not save without a name' do
+    @tag.name = nil
+    assert_not @tag.save
+  end
+
+  test 'should not save without a taggable' do
+    @tag.taggable = nil
+
+    assert_not @tag.save
+  end
+
+  test 'should be valid with all fields' do
+    assert @tag.valid?
+  end
+
+  test 'should save with all fields' do
+    assert @tag.save
+  end
+
+
 end
+
