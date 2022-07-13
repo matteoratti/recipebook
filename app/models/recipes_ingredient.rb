b103db0019335c85a95dafe820a00eb69612615c
jpsevern <matteoratti90@gmail.com>
2022-07-13 13:24:16 +0200

    create ingredient and recipe name with dirty callbacks

--- app/models/recipes_ingredient.rb
+++ app/models/recipes_ingredient.rb
@@ -3,4 +3,18 @@
 class RecipesIngredient < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient
+  
+  before_validation :set_recipe_name, :set_ingredient_name
+  
+  validates :recipe_name, :ingredient_name, presence: true
+
+  private 
+
+  def set_recipe_name
+    self.recipe_name = self.recipe.name
+  end
+
+  def set_ingredient_name
+    self.ingredient_name = self.ingredient.name
+  end
 end
