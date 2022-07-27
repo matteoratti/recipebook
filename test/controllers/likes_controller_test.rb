ee3cd6ea9ae2ca9c02c5ed5208dd8e364a61f0b2
jpsevern <matteoratti90@gmail.com>
2022-07-27 15:42:53 +0200

    add many signed in users can like the same recipe likes controller test

--- test/controllers/likes_controller_test.rb
+++ test/controllers/likes_controller_test.rb
@@ -7,6 +7,7 @@ class LikesControllerTest < ActionDispatch::IntegrationTest
 
   setup do
     @user1 = users(:user1)
+    @user2 = users(:user2)
     @recipe1 = recipes(:carbonara)
   end
 
@@ -30,5 +31,26 @@ class LikesControllerTest < ActionDispatch::IntegrationTest
     assert_response :redirect
 
     assert_redirected_to new_user_session_path
+  end
+
+  test 'many signed in users can like the same recipe' do
+    sign_in @user1
+
+    assert_difference('Like.count') do
+      post recipe_likes_url(@recipe1)
+    end
+
+    assert_response :success
+
+    sign_in @user2
+
+    assert_difference('Like.count') do
+      post recipe_likes_url(@recipe1)
+    end
+
+    assert_response :success
+
+    assert @user1.liked.exists?(likeable: @recipe1)
+    assert @user2.liked.exists?(likeable: @recipe1)
   end
 end
